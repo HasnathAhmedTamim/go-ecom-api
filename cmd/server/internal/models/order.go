f0d34c2dc0a085500a9cce88f9aea53b3cc8b60f
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-02-24 10:57:12 +0600

    update

--- cmd/server/internal/models/order.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Order struct {
-	ID       string         `json:"id"`
-	UserID   string         `json:"user_id"`
-	Products map[string]int `json:"products"` // productID -> quantity
-	Status   string         `json:"status"`
-}
