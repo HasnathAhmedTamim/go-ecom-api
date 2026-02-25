2983b519117256e52f5d49b3169ed81b350235b7
HasnathAhmedTamim <hasnath.tamim333@gmail.com>
2026-02-25 11:19:51 +0600

    frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div className='text-gray-500 text-center mt-10'>
+        Hello from App.jsx
+      </div>
+    </>
+  )
+}
+
+export default App
